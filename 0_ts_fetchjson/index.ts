89f2fd893943aed1e576de35f3476568ad5e4d85
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-03-26 20:12:24 +0100

    0. One Quick Change

--- 0_ts_fetchjson/index.ts
+++ 0_ts_fetchjson/index.ts
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response => {
-  console.log(response.data);
-})
+axios.get(url).then((response) => {
+  // console.log(response.data);
+  const { ID, Title, finished } = response.data;
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${Title}
+    Is it finished? ${finished}`);
+});
