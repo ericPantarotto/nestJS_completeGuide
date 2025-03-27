e6f4ab170c426f4422a56b2a14274569c34d5880
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-03-27 08:47:25 +0100

    0. catching more errors

--- 0_ts_fetchjson/index.ts
+++ 0_ts_fetchjson/index.ts
@@ -9,12 +9,13 @@ interface Todo {
 }
 
 axios.get(url).then((response) => {
-
   const { id, title, completed } = response.data as Todo;
+  logTodo(id, title, completed);
+});
 
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}`);
-});
-
+};
