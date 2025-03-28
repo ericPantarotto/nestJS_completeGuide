396df0d5cbdf8232d11bdedd3caea46988e18002
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-03-28 13:53:35 +0100

    0. annotations with variables

---
+++ 0_ts_types/annotations/variables.ts
@@ -0,0 +1,15 @@
+let apples: number = 5;
+apples = 10;
+//ERROR: apples = 'string ';
+
+const speed: string = 'fast';
+const hasName: boolean = true;
+
+const nothingMuch: null = null;
+const nothing: undefined = undefined;
+
+// built in objects
+const now: Date = new Date();
+
+console.log(apples, speed, hasName, nothingMuch, nothing, now);
+
