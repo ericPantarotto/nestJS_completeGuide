ca5a1db19e45f4a32095a8e8e4f6e066bf3d6c5d
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-04-12 13:49:59 +0200

    5. Reading and Writing to a Storage File

--- 5_messages_service_repo/src/messages/messages.repository.ts
+++ 5_messages_service_repo/src/messages/messages.repository.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
   async findOne(id: string): Promise<object> {
@@ -8,7 +8,21 @@ export class MessagesRepository {
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents) as { [key: string]: object };
 
-  // async create(message: string) {}
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents) as { [key: string]: object };
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
