9898c580b568e42c0539f1ecc5e619e6d5c4bcb6
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-04-14 18:09:00 +0200

    6.  More on DI Between Modules

--- 6_organize_modules/src/cpu/cpu.service.ts
+++ 6_organize_modules/src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number): number {
+    console.log('Drawing watts of power from power service');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
--- 6_organize_modules/src/disk/disk.service.ts
+++ 6_organize_modules/src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getData(): string {
+    console.log('Drawing watts of power from power service');
+    this.powerService.supplyPower(20);
+    return 'Disk data';
+  }
+}
