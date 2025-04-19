e8a86503bfb09045efe326cbe9e53bc802511ba7
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-04-19 19:59:57 +0200

    9. Updating Records

--- 9_carvalue_creating-saving-data/src/users/users.controller.ts
+++ 9_carvalue_creating-saving-data/src/users/users.controller.ts
@@ -4,10 +4,12 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -36,5 +38,10 @@ export class UsersController {
   @Delete('/:id')
   async deleteUser(@Param('id') id: string) {
     return await this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return await this.usersService.update(parseInt(id), body);
   }
 }
