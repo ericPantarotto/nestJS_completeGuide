15237c76da6f44883b8f74f3e1eab530cabc6dec
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-04-20 10:52:44 +0200

    10.  Customizing the Interceptor's DTO

--- 10_carvalue_custom-data/src/interceptors/serialize.interceptor.ts
+++ 10_carvalue_custom-data/src/interceptors/serialize.interceptor.ts
@@ -2,13 +2,14 @@ import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        return plainToInstance(UserDto, data, {
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
--- 10_carvalue_custom-data/src/users/users.controller.ts
+++ 10_carvalue_custom-data/src/users/users.controller.ts
@@ -15,6 +15,7 @@ import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -30,7 +31,7 @@ export class UsersController {
   }
 
   // @UseInterceptors(ClassSerializerInterceptor)
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
