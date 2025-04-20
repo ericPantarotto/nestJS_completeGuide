3e2af6447ce7a0671ece657045ce887a073748e9
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-04-20 11:01:33 +0200

    10. Controller-Wide Serialization

--- 10_carvalue_custom-data/src/users/users.controller.ts
+++ 10_carvalue_custom-data/src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -31,7 +32,7 @@ export class UsersController {
 
   // @UseInterceptors(ClassSerializerInterceptor)
   // @UseInterceptors(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
+  // @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
