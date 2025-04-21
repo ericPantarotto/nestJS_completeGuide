00442ffc5d11f62d3a36690098bbd2b37414036b
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-04-21 22:56:17 +0200

    11. module setup

---
+++ 11_carvalue_authentication/src/users/users.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  // ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
+import { UsersService } from './users.service';
+
+@Serialize(UserDto)
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+
+    await this.usersService.create(body.email, body.password);
+
+    return { message: 'User created' };
+  }
+
+  // @UseInterceptors(ClassSerializerInterceptor)
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  // @Serialize(UserDto)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException(`User not found with id: ${id}`);
+    return user;
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    return await this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return await this.usersService.update(parseInt(id), body);
+  }
+}
