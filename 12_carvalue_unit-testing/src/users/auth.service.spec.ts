f18b81c0473dfa54b37dd38282b6b94336f6746f
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-04-24 22:17:41 +0200

    12. Changing Mock Implementations

--- 12_carvalue_unit-testing/src/users/auth.service.spec.ts
+++ 12_carvalue_unit-testing/src/users/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
@@ -5,9 +6,11 @@ import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
   beforeEach(async () => {
     //NOTE: Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -34,5 +37,16 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        { id: 1, email: 'userfound@test.com', password: 'test1234' } as User,
+      ]);
+
+    await expect(
+      service.signup('userfound@test.com', 'test1234'),
+    ).rejects.toThrow(BadRequestException);
   });
 });
