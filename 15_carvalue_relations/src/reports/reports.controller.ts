e4d1bfa5f7469ea6a107e1c4e166928c86ce3b55
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-05-01 19:48:59 +0200

    15. Receiving Report Creation Requests

--- 15_carvalue_relations/src/reports/reports.controller.ts
+++ 15_carvalue_relations/src/reports/reports.controller.ts
@@ -1,8 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @UseGuards(AuthGuard)
   @Post()
-  createReport(@Body() body: CreateReportDto) {}
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
 }
