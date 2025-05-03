6358869f0cbe623a400cce9921133398a0716d5f
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-05-03 22:18:12 +0200

    16. Adding in Report Approval

--- 16_carvalue_rp-approval/src/reports/reports.controller.ts
+++ 16_carvalue_rp-approval/src/reports/reports.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Patch, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
@@ -16,5 +17,14 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AuthGuard)
+  approveReport(
+    @Param('id') id: string,
+    @Body('approved') body: ApproveReportDto,
+  ) {
+    return this.reportsService.approve(id, body);
   }
 }
