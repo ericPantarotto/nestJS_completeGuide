b1404a31efa6e07f97d537fad0bbe831baa7134d
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-05-08 12:50:17 +0200

    18. pre-production changes

---
+++ 18_carvalue_deployment/ormconfig.js
@@ -0,0 +1,42 @@
+import { DataSource } from 'typeorm';
+
+const dbConfig = {
+  synchronize: false,
+  migrations: ['dist/migrations/*.js'],
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+      migrationsRun: true,
+      keepConnectionAlive: true,
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    throw new Error('unknown env');
+}
+
+const dtSource = new DataSource(dbConfig);
+
+export { dbConfig, dtSource };
--- 18_carvalue_deployment/src/app.module.ts
+++ 18_carvalue_deployment/src/app.module.ts
@@ -3,11 +3,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import cookieSession from 'cookie-session';
+// import { dataSourceOptions } from 'typeorm.config';
+import { dbConfig } from '../ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Report } from './reports/report.entity';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -16,15 +16,8 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'sqlite',
-        database: configService.get('DB_NAME') || 'db.sqlite',
-        entities: [User, Report],
-        synchronize: true,
-      }),
-    }),
+    // TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forRoot(dbConfig),
     UsersModule,
     ReportsModule,
   ],
